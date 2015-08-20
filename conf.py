2b0023ef5d2fd2ffc40990b25db20154517cb1af
Danch <daniel@global-resonance.com>
2015-08-20 14:04:15 +0100

    Before I break the django site with jquery

--- conf.py
+++ conf.py
@@ -1,4 +1,8 @@
-model_path = "/srv/repos/word2vec-api/models/GoogleNews-vectors-negative300.bin"
+import os
+if os.name == 'nt':
+    model_path = 'F:\Dropbox\word2vec-api\models\GoogleNews-vectors-negative300.bin'
+else:
+    model_path = "/srv/repos/word2vec-api/models/GoogleNews-vectors-negative300.bin"
 binary = True
 host = "localhost"
 port = 3031
